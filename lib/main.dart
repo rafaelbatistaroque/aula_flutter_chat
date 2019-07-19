d671bf403788df46412088e14fdf81bc28aa7fa6
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-18 22:26:59 -0400

    Added settings to Firebase and tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-main() => runApp(
-  MaterialApp(
-    title: "App chat",
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  )
-);
+main(){
+
+Firestore.instance.collection("Teste").document().setData({"teste" : "teste"});
+
+  runApp(
+    MaterialApp(
+      title: "App chat",
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    )
+  );
+}
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
