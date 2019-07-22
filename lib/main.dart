a5b45c5bacc6256e0734bc3763493bbbd629b4e4
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-22 14:20:03 -0400

    Settings theme colors to IOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
+import 'package:aula_flutter_chat/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-main(){
+main() async => runApp(MyApp());
 
-Firestore.instance.collection("Teste").document().setData({"teste" : "teste"});
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
 
-  runApp(
-    MaterialApp(
-      title: "App chat",
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    )
-  );
-}
-class Home extends StatelessWidget {
+final ThemeData kdefaultTheme = ThemeData(
+    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: "App chat",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kdefaultTheme,
+      home: Home(),
     );
   }
 }
-
