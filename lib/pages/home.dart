456728976ac5d03df1d2fb13244a76575ead826b
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-22 14:28:47 -0400

    Added builder AppBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,20 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: _buildAppBar(),
+      ),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      title: Text("Chat App"),
+      centerTitle: true,
+      elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+    );
   }
 }
